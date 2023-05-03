98d69fed2127022fd878e63cc037de3a53e3782a
hasankarim18 <hallysust@gmail.com>
2023-05-03 16:36:43 +0600

    recipe json data created and selected route created

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5000;
-const recipe = require('./recipe/recipe.json')
+const recipes = require('./recipe/recipe.json')
+// const chefs = require('./chefs/chejs.json')
 
 app.use(cors());
 
@@ -10,8 +11,25 @@ app.get("/", (req, res) => {
   res.send("World indian spicy recipe comming soon....");
 });
 
-app.get("/recipe", (req, res) => {
-  res.send(recipe);
+// app.get('/chefs', (req, res)=> {
+//   res.send(chefs)
+// } )
+
+// app.get('/chefs/:id', (req, res)=> {
+//    const id = req.params.id;
+//    const selectedChefs = chefs.find((r) => r.chef_id === id);
+//    res.send(selectedChefs);
+// } )
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedRecipies = recipes.find((r) => r.chef_id === id);
+  res.send(selectedRecipies)
 });
 
 app.listen(port, () => {
